757ef8cb4de1934f4c831a40069b7d6e0b1b464f
sumitnegikv369 <sumit.negi@successive.tech>
2024-04-23 00:51:31 +0530

    assignment 2 get api created for returning mock list

---
+++ Assignment-2/mockData.js
@@ -0,0 +1,17 @@
+const mockList = [
+    {
+      id: 1,
+      name: "Item 1",
+      description: "This is the description of item 1.",
+    },
+    {
+      id: 2,
+      name: "Item 2",
+      description: "This is the description of item 2.",
+    },
+  ];
+  
+module.exports = {
+    mockList
+}
+  
