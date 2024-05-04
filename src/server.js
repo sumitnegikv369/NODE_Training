88e7d76a5a4b512e07da7d62c8de36823e55d6f6
sumitnegikv369 <sumit.negi@successive.tech>
2024-05-04 16:12:34 +0530

    folder structure changed

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const userRouter = require("./router/userRouter");
+const userRouter2 = require("./router/userRouter2");
 const app = express();
 
 const server = () => {
     app.use(express.json());
     app.use("/api",userRouter);
+    app.use("/api",userRouter2);
     return app;
 }
 
