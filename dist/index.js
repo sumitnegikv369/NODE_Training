3d21f492c6e89ff6d965a95d708a8f961790dc3e
sumit negi <161808037+sumitnegikv369@users.noreply.github.com>
2024-05-08 10:42:41 +0530

    Delete dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-require("dotenv").config();
-const serverSetup = require("./serverSetup");
-const PORT = process.env.PORT || 4001;
-serverSetup().listen(PORT, () => {
-    console.log(`Server started on PORT: ${PORT}`);
-});
